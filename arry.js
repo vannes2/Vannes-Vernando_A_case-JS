f82252bc83c069ca18b1adf4427a0ef73ad002e8
vannes2 <vanesvernando72@gmail.com>
2024-11-07 12:11:16 +0700

    first commit

---
+++ arry.js
@@ -0,0 +1,9 @@
+let antrian = ["ray", "fiki", "fadhilla", "farah"];
+antrian.push("nabila");
+antrian.push("maza", "elsi");
+antrian.pop();
+antrian.shift();
+antrian.shift();
+antrian.unshift("tomi");
+
+console.log(antrian);
